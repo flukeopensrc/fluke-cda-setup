0446e4899985b2423e741300fe5f983dd210fe79
Prosper Van <prosper.van@fluke.com>
2023-06-09 17:07:14 -0700

    allow use of system repo tool

--- setup-angstrom-manifest.sh
+++ setup-angstrom-manifest.sh
@@ -2,31 +2,46 @@
 set -e
 
 if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 ANGSTROM_BRANCH" >&2
-  exit 1
+    echo "Usage: $0 ANGSTROM_BRANCH" >&2
+    exit 1
 fi
 
 ANGSTROM_BRANCH="$1"
 MAN_DIR="${ANGSTROM_BRANCH}-manifest"
 
 if [ -d $MAN_DIR ]; then
-	echo "$MAN_DIR exists"
-	exit 1
+    echo "$MAN_DIR exists"
+    exit 1
 fi
 
-# the version of repo in Ubuntu 18.04.6 LTS is broken, so try to
-# bootstrap up to a working version.
-mkdir -p bin
-curl https://storage.googleapis.com/git-repo-downloads/repo > bin/repo
-chmod a+rx bin/repo
+REPO_PATH=""
+if command -v repo &> /dev/null
+then
+    echo "Google repo found"
+    REPO_PATH="$(command -v repo)"
+else
+    echo "Google repo NOT found"
+    # the version of repo in Ubuntu 18.04.6 LTS is broken (maybe not anymore?), so try to
+    # bootstrap up to a working version.
+    mkdir -p bin
+    curl https://storage.googleapis.com/git-repo-downloads/repo > bin/repo
+    chmod a+rx bin/repo
+    REPO_PATH="./bin/repo"
+fi
+
 
 mkdir $MAN_DIR
 cd $MAN_DIR
 
+# echo "Using repo from: ${REPO_PATH}"
+# python3 ./bin/repo init -u https://github.com/flukeopensrc/cda-manifest -b ${ANGSTROM_BRANCH} ||
+#     python3 ./bin/repo init -u https://github.com/flukeopensrc/cda-manifest -b ${ANGSTROM_BRANCH}
+# python3 .repo/repo/repo sync
 
-../bin/repo init -u https://github.com/flukeopensrc/cda-manifest -b ${ANGSTROM_BRANCH} ||
-    python3 ../bin/repo init -u https://github.com/flukeopensrc/cda-manifest -b ${ANGSTROM_BRANCH}
-python3 .repo/repo/repo sync
+echo "Using repo from: ${REPO_PATH}"
+${REPO_PATH} init -u https://github.com/flukeopensrc/cda-manifest -b ${ANGSTROM_BRANCH} ||
+    ${REPO_PATH} init -u https://github.com/flukeopensrc/cda-manifest -b ${ANGSTROM_BRANCH}
+${REPO_PATH} sync
 
 MACHINE=fluke-cda-nighthawk source ./setup-environment ""
 
